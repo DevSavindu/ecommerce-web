9f6dc473d47eeced9fe609db3e6acbb3561af48b
it21307126 <it21307126@my.sliit.lk>
2024-02-15 00:39:15 +0530

    frontend

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,46 +27,60 @@ const ImageList = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
   },
 ];
+
 const Hero = () => {
+  var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
+
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
       {/* bckgrnd pattern */}
       <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9 "></div>
       {/* hero section */}
       <div className="container pb-8 sm:pb-0">
-        <Slider>
-        <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2">
-            {/* text */}
-            <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10 ">
-              <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
-                Lorem Ipsum is simply dummy text
-              </h1>
-              <p className="text-sm">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry
-              </p>
-              <div>
-                <button className="bg-gradient-to-r from-primary to-secondary hover:scale-10 transition-all duration-200 text-white py-2 px-4 rounded-full ">
-                  Order now
-                </button>
+        <Slider {...settings}>
+          {ImageList.map((data) => (
+            <div key={data.id}>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text */}
+                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10 ">
+                  <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                    {data.title}
+                  </h1>
+                  <p className="text-sm">
+                    {data.description}
+                  </p>
+                  <div>
+                    <button className="bg-gradient-to-r from-primary to-secondary hover:scale-10 transition-all duration-200 text-white py-2 px-4 rounded-full ">
+                      Order now
+                    </button>
+                  </div>
+                </div>
+                {/* image */}
+                <div className="order-1 sm:order-2">
+                  <div className="relative z-10">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px]  sm:scale-125 object-contain mx-auto"
+                    />
+                  </div>
+                </div>
+                {/* text */}
               </div>
             </div>
-            {/* image */}
-            <div className=" order-1 sm:order-2">
-              <div className="relative z-10">
-                <img
-                  src={Image1}
-                  alt=""
-                  className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px]  sm:scale-125 object-contain mx-auto"
-                />
-              </div>
-            </div>
-            {/* text */}
-          </div>
-        </div>
+          ))}
         </Slider>
-        
       </div>
     </div>
   );
