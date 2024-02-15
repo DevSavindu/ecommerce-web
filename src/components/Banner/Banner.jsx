999017ba33fbeea80a1e4ff48404d4645e99f9cd
it21307126 <it21307126@my.sliit.lk>
2024-02-15 21:41:59 +0530

    frontend

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='min-h-[550px] flex justify-center items-center py-12 sm:py-0'>
+      <div className='container'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-6 items-center '>
+            image
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Banner
