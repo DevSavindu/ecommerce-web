fc8713fd6f8f6659fffe64da3435dbf074c059ea
it21307126 <it21307126@my.sliit.lk>
2024-02-15 23:10:28 +0530

    frontend

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -18,7 +18,47 @@ const Banner = () => {
                 />
             </div>
             {/* text details */}
-
+                <div className='flex flex-col justify-center gap-6 sm:pt-0'>
+                    <h1 
+                    data-aos="fade-up"
+                    className='text-3xl sm:text-4xl font-bold '>
+                        Winter Sale upto 50% off</h1>
+                    <p 
+                    data-aos="fade-up"
+                    className='text-sm text-gray-500 tracking-wide leading-5'>
+                        Lorem ipsum dolor sit, 
+                        amet consectetur adipisicing elit. 
+                        Reiciendis laboriosam, q
+                        uaerat explicabo perferendis nemo 
+                        consectetur totam laudantium quisquam 
+                    </p>
+                    <div className='flex flex-col gap-4'>
+                        <div 
+                        data-aos="fade-up"
+                        className='flex items-center gap-4'>
+                            <GrSecure className='text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-violet-100 dark:bg-violet-400'/>
+                            <p>Quality Products</p>
+                        </div>
+                        <div 
+                        data-aos="fade-up"
+                        className='flex items-center gap-4'>
+                            <IoFastFood className='text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-orange-100 dark:bg-orange-400'/>
+                            <p>Fast Delivery</p>
+                        </div>
+                        <div 
+                        data-aos="fade-up"
+                        className='flex items-center gap-4'>
+                            <GiFoodTruck className='text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-green-100 dark:bg-green-400'/>
+                            <p>Easy Payment Method</p>
+                        </div>
+                        <div 
+                        data-aos="fade-up"
+                        className='flex items-center gap-4'>
+                            <GiFoodTruck className='text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-yellow-100 dark:bg-yellow-400'/>
+                            <p>Get Offers</p>
+                        </div>
+                    </div>
+                </div>
         </div>
 
       </div>
