b73740eca501cf8e869616ef18ff9dcbdbfbc33f
it21307126 <it21307126@my.sliit.lk>
2024-02-15 22:36:29 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from './components/Products/Products'
 import AOS from "aos"
 import "aos/dist/aos.css"
 import TopProducts from './components/TopProducts/TopProducts'
+import Banner from './components/Banner/Banner'
 
 const App = () => {
   React.useEffect(() => {
@@ -24,6 +25,7 @@ const App = () => {
       <Hero/>
       <Products/>
       <TopProducts/>
+      <Banner/>
     </div>
   )
 }
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import BannerImg from "../../assets/banner/lady-pink-suit-looking-camera.jpg"
+import { GrSecure } from "react-icons/gr"
+import {IoFastFood } from "react-icons/io5"
+import { GiFoodTruck } from "react-icons/gi"
 
 const Banner = () => {
   return (
     <div className='min-h-[550px] flex justify-center items-center py-12 sm:py-0'>
       <div className='container'>
         <div className='grid grid-cols-1 sm:grid-cols-2 gap-6 items-center '>
-            image
+            {/* image */}
+            <div data-aos="zoom-in">
+                <img 
+                src={BannerImg} 
+                alt="" 
+                className='max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover'
+                />
+            </div>
+            {/* text details */}
 
         </div>
 
